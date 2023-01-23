ae814a35cdc831d9c3c881155ddc1f4983ebef23
@foxstudent103527 <pistron3000@gmail.com>
2023-01-23 17:14:04 +0600

    edit6 unit test

--- app/src/test/java/com/example/tast1/ReverseTest.java
+++ app/src/test/java/com/example/tast1/ReverseTest.java
@@ -12,6 +12,10 @@ public class ReverseTest {
             new TestObject("dcb1a hgfle", "a1bcd efglh", "xl"),
             new TestObject("qw@!321e qCBAqq", "qwe123!@ qABCqq", "qw"),
             new TestObject("y2trweq31 !q2", "123qwerty q!2", "e2"),
+            new TestObject("qwe123 !@ABCqq", "ewq123 !@qqCBA", ""),
+            new TestObject("dcba hgfe", "abcd efgh", ""),
+            new TestObject("dednimxoF looc 24/7", "Foxminded cool 24/7", ""),
+            new TestObject("d1cba hgf!e", "a1bcd efg!h", ""),
     };
 
 
